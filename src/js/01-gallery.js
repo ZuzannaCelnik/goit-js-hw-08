cc56b63e35d5e2308c0970941c798c268d54bb44
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2022-08-04 16:39:18 +0200

    01 gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,23 +5,28 @@ import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector("div.gallery");
-const galleryItemsElement = galleryItems
-      .map(element => {
-            const image = `<a class="gallery__item" href="${element.original}" >
+const galleryList =  document.querySelector(".gallery");
+
+const galleryItemsElement = galleryItems.map(element => {
+            const image = `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}" >
     <img
       class="gallery__image"
       src="${element.preview}"
+      data-source="${element.original}"
       alt="${element.description}"
-      title="${element.description}"
     />
-  </a>`;
+  </a>
+</div>`;
       return image;
       })
       .join(" ");
 
-gallery.insertAdjacentHTML("afterbegin", galleryItemsElement);
+      
+galleryList.insertAdjacentHTML("beforeend", galleryItemsElement);  
 
-new SimpleLightbox(".gallery a", { captionDelay: 250 });
-
-console.log(galleryItems);
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionClass: "bottom",
+    captionDelay: "250",
+});
