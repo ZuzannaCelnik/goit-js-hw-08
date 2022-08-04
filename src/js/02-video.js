67966b0bf6f320e17ddf0ddce98d78d6daeae5e8
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2022-08-04 17:22:07 +0200

    task 2 updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  player.setCurrentTime(savedData);
+}
